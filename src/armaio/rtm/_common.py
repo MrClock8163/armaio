48054e3017c2ef5c5ab3eb3cfffbfdc8433cf210
MrClock8163 <48679831+MrClock8163@users.noreply.github.com>
2026-02-01 20:08:24 +0100

    [rtm] moved transform conversion to common module

--- src/armaio/rtm/_common.py
+++ src/armaio/rtm/_common.py
@@ -35,3 +35,32 @@ class Bone(NamedTuple):
 
 BoneStructure: TypeAlias = dict[str, 'BoneStructure']
 BoneSequence: TypeAlias = tuple[Bone, ...]
+
+
+def _rot_loc_to_matrix(q: RtmQuaternion, v: RtmVector) -> RtmMatrix:
+    """
+    Convertes a quaternion-vector pair to matrix representation.
+
+    :param q: Orientation
+    :type q: RtmQuaternion
+    :param v: Position
+    :type v: RtmVector
+    :return: Transformation matrix
+    :rtype: RtmMatrix
+    """
+    m00 = 1 - 2*q.y**2 - 2*q.z**2
+    m01 = 2*q.x*q.y - 2*q.z*q.w
+    m02 = 2*q.x*q.z + 2*q.y*q.w
+    m10 = 2*q.x*q.y + 2*q.z*q.w
+    m11 = 1 - 2*q.x**2 - 2*q.z**2
+    m12 = 2*q.y*q.z - 2*q.x*q.w
+    m20 = 2*q.x*q.z - 2*q.y*q.w
+    m21 = 2*q.y*q.z + 2*q.x*q.w
+    m22 = 1 - 2*q.x**2 - 2*q.y**2
+
+    return (
+        (m00, -m01, m02, 0.0),
+        (-m10, m11, -m12, 0.0),
+        (m20, -m21, m22, 0.0),
+        (-v.x, v.y, -v.z, 1.0)
+    )
--- src/armaio/rtm/_rtm.py
+++ src/armaio/rtm/_rtm.py
@@ -14,8 +14,8 @@ from ._common import (
     BoneSequence,
     RtmProperty,
     RtmVector,
-    RtmQuaternion,
-    RtmMatrix
+    RtmMatrix,
+    _rot_loc_to_matrix
 )
 
 
@@ -190,25 +190,6 @@ class RtmFrame:
                     mat
                 )
 
-    @staticmethod
-    def _rot_loc_to_matrix(q: RtmQuaternion, v: RtmVector) -> RtmMatrix:
-        m00 = 1 - 2*q.y**2 - 2*q.z**2
-        m01 = 2*q.x*q.y - 2*q.z*q.w
-        m02 = 2*q.x*q.z + 2*q.y*q.w
-        m10 = 2*q.x*q.y + 2*q.z*q.w
-        m11 = 1 - 2*q.x**2 - 2*q.z**2
-        m12 = 2*q.y*q.z - 2*q.x*q.w
-        m20 = 2*q.x*q.z - 2*q.y*q.w
-        m21 = 2*q.y*q.z + 2*q.x*q.w
-        m22 = 1 - 2*q.x**2 - 2*q.y**2
-
-        return (
-            (m00, -m01, m02, 0.0),
-            (-m10, m11, -m12, 0.0),
-            (m20, -m21, m22, 0.0),
-            (-v.x, v.y, -v.z, 1.0)
-        )
-
     @classmethod
     def from_binarized(
         cls,
@@ -224,7 +205,7 @@ class RtmFrame:
             rot, loc = transform
             frame_rtm.set_transform(
                 bone,
-                cls._rot_loc_to_matrix(rot, loc)
+                _rot_loc_to_matrix(rot, loc)
             )
 
         if isinstance(skeleton, dict):
