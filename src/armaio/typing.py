f9b20764d92cbd1731f5ed0ba4d2cc5ed87b017f
MrClock <48679831+MrClock8163@users.noreply.github.com>
2026-02-08 20:01:32 +0100

    [core] added typing module with path type aliases

---
+++ src/armaio/typing.py
@@ -0,0 +1,7 @@
+from os import PathLike
+from typing import TypeAlias
+
+
+StrPath: TypeAlias = str | PathLike[str]
+BytesPath: TypeAlias = bytes | PathLike[bytes]
+StrOrBytesPath: TypeAlias = StrPath | BytesPath
